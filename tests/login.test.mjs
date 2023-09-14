49a304e6ad483d14a17cf01bc6eb403db74c8b1b
Evan Prodromou <evan@openearth.org>
2023-09-14 15:44:03 +0200

    feat: login by actorid, webfinger
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- tests/login.test.mjs
+++ tests/login.test.mjs
@@ -1,4 +1,4 @@
-import { describe } from 'node:test'
+import { describe, it } from 'node:test'
 import { parseArgs } from 'node:util'
 import { join } from 'node:path'
 import { readFileSync } from 'node:fs'
@@ -9,6 +9,8 @@ import { Command } from '../command.mjs'
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
 describe('Login command', async () => {
+
+  it('Login by webfinger', async() => {
     const webfinger = 'evanp@localhost:3000'
     const argv = ['login', webfinger]
     const args = parseArgs({args: argv, allowPositionals: true})
@@ -17,4 +19,16 @@ describe('Login command', async () => {
     const tokens = JSON.parse(readFileSync(join(process.env.HOME, '.ap-tokens')))
     const actorId = await Command.fetchActorId(webfinger)
     assert.ok(tokens[actorId])
+  })
+
+  it('Login by actor ID', async () => {
+    const webfinger = 'evanp@localhost:3000'
+    const actorId = await Command.fetchActorId(webfinger)
+    const argv = ['login', actorId]
+    const args = parseArgs({args: argv, allowPositionals: true})
+    const command = new LoginCommand(args)
+    await command.run()
+    const tokens = JSON.parse(readFileSync(join(process.env.HOME, '.ap-tokens')))
+    assert.ok(tokens[actorId])
+  })
 })
