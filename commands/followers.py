8d0a4942005269520bfade2bec08cb6db14407e8
Evan Prodromou <evan@openearth.org>
2023-10-22 16:13:24 -0400

    feat: correct contents for followers, following
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- commands/followers.py
+++ commands/followers.py
@@ -14,16 +14,13 @@ class FollowersCommand(Command):
         slice = self.collection_slice(coll, self.offset, self.limit)
         rows = []
         for item in slice:
-            activity = self.to_object(item, ['id', 'actor', 'published'])
             follower = self.to_object(
-                activity['actor'],
+                item,
                 [ 'id',
                   'preferredUsername',
                   ['name', 'nameMap', 'summary', 'summaryMap'] ]
             )
-            activity_id = self.to_id(activity)
             id = self.to_webfinger(follower)
             name = self.to_text(follower)
-            published = activity['published'] if 'published' in activity else None
-            rows.append([activity_id, id, name, published])
-        print(tabulate(rows, headers=['activity', 'id', 'name', 'published']))
+            rows.append([id, name])
+        print(tabulate(rows, headers=['id', 'name']))
