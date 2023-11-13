f870118cdcf9e24026bf9318d7a683cfebbb71eb
Evan Prodromou <evan@openearth.org>
2023-11-12 23:48:05 -0500

    test: create note command

---
+++ tests/test_create_note.py
@@ -0,0 +1,141 @@
+import unittest
+from unittest.mock import patch, mock_open, MagicMock
+from commands.create_note import CreateNoteCommand
+from argparse import Namespace
+import io
+import sys
+import requests
+from requests_oauthlib import OAuth2Session
+import json
+
+ACTOR_ID = "https://social.example/users/evanp"
+OTHER_ID = "https://social.example/users/other"
+CREATE_ID = f"{ACTOR_ID}/create/1"
+CONTENT = "Hello, world!"
+
+ACTOR = {
+    "type": "Person",
+    "id": ACTOR_ID,
+    "outbox": f"{ACTOR_ID}/outbox",
+    "followers": f"{ACTOR_ID}/followers",
+    "endpoints": {"proxyUrl": "https://social.example/proxy"},
+}
+
+OTHER = {
+    "type": "Person",
+    "id": OTHER_ID,
+    "outbox": f"{OTHER_ID}/outbox",
+    "followers": f"{OTHER_ID}/followers",
+    "endpoints": {"proxyUrl": "https://social.example/proxy"},
+}
+
+TOKEN_FILE_DATA = json.dumps({"actor_id": ACTOR_ID, "access_token": "12345678"})
+
+
+def mock_oauth_get(url, headers=None):
+    if url == ACTOR_ID:
+        return MagicMock(status_code=200, json=lambda: ACTOR)
+    else:
+        return MagicMock(status_code=404)
+
+
+def mock_oauth_post(url, headers=None, data=None):
+    if url == ACTOR["outbox"]:
+        added_data = {
+            "id": CREATE_ID,
+            "published": "2020-01-01T00:00:00Z",
+            "actor": ACTOR_ID,
+        }
+        return MagicMock(
+            status_code=200, json=lambda: {**json.loads(data), **added_data}
+        )
+    else:
+        return MagicMock(status_code=404)
+
+
+class TestCreateNoteCommand(unittest.TestCase):
+    def setUp(self):
+        self.held, sys.stdout = sys.stdout, io.StringIO()  # Redirect stdout
+
+    def tearDown(self):
+        sys.stdout = self.held
+
+    @patch("builtins.open", new_callable=mock_open, read_data=TOKEN_FILE_DATA)
+    @patch("requests_oauthlib.OAuth2Session.post", side_effect=mock_oauth_post)
+    @patch("requests_oauthlib.OAuth2Session.get", side_effect=mock_oauth_get)
+    def test_create_note_public(self, mock_requests_get, mock_requests_post, mock_file):
+        args = Namespace(
+            subcommand="create",
+            subsubcommand="note",
+            content=[CONTENT],
+            public=True,
+            private=False,
+            followers_only=False,
+            to=[],
+            cc=[],
+        )
+        cmd = CreateNoteCommand(args)
+
+        cmd.run()
+
+        # Assertions
+        self.assertGreaterEqual(mock_requests_get.call_count, 1)
+        self.assertGreaterEqual(mock_requests_post.call_count, 1)
+        self.assertIn(CONTENT, sys.stdout.getvalue())
+        self.assertIn("Public", sys.stdout.getvalue())
+
+    @patch("builtins.open", new_callable=mock_open, read_data=TOKEN_FILE_DATA)
+    @patch("requests_oauthlib.OAuth2Session.post", side_effect=mock_oauth_post)
+    @patch("requests_oauthlib.OAuth2Session.get", side_effect=mock_oauth_get)
+    def test_create_note_followers_only(
+        self, mock_requests_get, mock_requests_post, mock_file
+    ):
+        args = Namespace(
+            subcommand="create",
+            subsubcommand="note",
+            content=[CONTENT],
+            public=False,
+            private=False,
+            followers_only=True,
+            to=[],
+            cc=[],
+        )
+        cmd = CreateNoteCommand(args)
+
+        cmd.run()
+
+        # Assertions
+        self.assertGreaterEqual(mock_requests_get.call_count, 1)
+        self.assertGreaterEqual(mock_requests_post.call_count, 1)
+        self.assertIn(CONTENT, sys.stdout.getvalue())
+        self.assertIn(ACTOR["followers"], sys.stdout.getvalue())
+
+    @patch("builtins.open", new_callable=mock_open, read_data=TOKEN_FILE_DATA)
+    @patch("requests_oauthlib.OAuth2Session.post", side_effect=mock_oauth_post)
+    @patch("requests_oauthlib.OAuth2Session.get", side_effect=mock_oauth_get)
+    def test_create_note_private(
+        self, mock_requests_get, mock_requests_post, mock_file
+    ):
+        args = Namespace(
+            subcommand="create",
+            subsubcommand="note",
+            content=[CONTENT],
+            public=False,
+            private=True,
+            followers_only=False,
+            to=[OTHER_ID],
+            cc=[],
+        )
+        cmd = CreateNoteCommand(args)
+
+        cmd.run()
+
+        # Assertions
+        self.assertGreaterEqual(mock_requests_get.call_count, 1)
+        self.assertGreaterEqual(mock_requests_post.call_count, 1)
+        self.assertIn(CONTENT, sys.stdout.getvalue())
+        self.assertIn(OTHER_ID, sys.stdout.getvalue())
+
+
+if __name__ == "__main__":
+    unittest.main()
