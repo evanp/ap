04d25544f1625ea6d83a3e9cf951e33478dee715
Evan Prodromou <evan@openearth.org>
2023-11-19 13:06:34 -0500

    chore: correct semver in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ap',
-    version='0.1',
+    version='0.1.0-dev',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
